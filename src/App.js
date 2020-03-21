20eff5b3ac4ccae29d1407f0e6665a88646f2fe8
unknown <53538316+grlucy@users.noreply.github.com>
2020-03-20 20:09:31 -0400

    created friends map in App.js

--- src/App.js
+++ src/App.js
@@ -4,30 +4,29 @@ import Wrapper from "./components/Wrapper";
 import friends from "./friends.json";
 import "./App.css";
 
-function App() {
-  return (
-    <Wrapper>
-      <h1 className="title">Friends List</h1>
-      <FriendCard
-        name={friends[0].name}
-        image={friends[0].image}
-        occupation={friends[0].occupation}
-        location={friends[0].location}
-      />
-      <FriendCard
-        name={friends[1].name}
-        image={friends[1].image}
-        occupation={friends[1].occupation}
-        location={friends[1].location}
-      />
-      <FriendCard
-        name={friends[2].name}
-        image={friends[2].image}
-        occupation={friends[2].occupation}
-        location={friends[2].location}
-      />
-    </Wrapper>
-  );
+class App extends React.Component {
+  state = {
+    friends
+  };
+
+  //map over `this.state.friends` to render each `FriendCard` component
+  render() {
+    return (
+      <Wrapper>
+        <h1 className="title">Friends List</h1>
+        {this.state.friends.map(friends => (
+          <FriendCard
+            name={friends.name}
+            image={friends.image}
+            occupation={friends.occupation}
+            location={friends.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
+
+// }
 
 export default App;
